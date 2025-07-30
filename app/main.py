fe5ce7bbaab2803b75b5af360de369ea469744a9
eedo_y <doup2001@naver.com>
2025-07-30 11:16:31 +0900

    ✨ feat : 홈화면 추가 및 라우터 등록

---
+++ app/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+from app.api.qa import router as qa_router
+from app.api.home import router as home_router
+
+app = FastAPI(title="SEEAT AI API")
+
+# qa.py에서 만든 라우터를 앱에 등록
+app.include_router(qa_router, prefix="/api/v1/qa",)
+
+# 홈화면 라우터를 앱에 등록
+app.include_router(home_router)
