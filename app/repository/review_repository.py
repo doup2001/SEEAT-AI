9ef2f989b14aa1e2db8471931cac25782556fc40
eedo_y <doup2001@naver.com>
2025-08-06 17:24:27 +0900

    ✨ feat : 좌석 및 해시태그 한번에 가져오도록 수정

--- app/repository/review_repository.py
+++ app/repository/review_repository.py
@@ -1,8 +1,18 @@
+# app/repository/review_repository.py
+
+from typing import Type
 from sqlalchemy.orm import Session, joinedload
 from app.models.review import Review
 
-def get_all_reviews(db: Session) -> list[Review]:
+def get_all_reviews(db: Session) -> list[Type[Review]]:
     """
-    DB에서 모든 Review 객체와 연관된 해시태그 리스트까지 함께 조회합니다.
+    DB에서 모든 Review 객체와 연관된 해시태그 및 좌석 정보를 함께 조회합니다.
     """
-    return db.query(Review).options(joinedload(Review.hashtags)).all()
+    return (
+        db.query(Review)
+        .options(
+            joinedload(Review.hashtags),    # 해시태그 정보 즉시 로딩
+            joinedload(Review.seats)              # 좌석 정보 즉시 로딩
+        )
+        .all()
+    )
